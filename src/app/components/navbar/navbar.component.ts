8ccdad1ff29a9f04ab0c74b2de19be9a2b6d034b
lixuanqi <lixuanqi1995@gmail.com>
2019-01-24 00:21:38 -0800

    add github and linkedin url to About tag in navbar.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LINKEDIN_URL, GITHUB_URL } from '../../../profile';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+
+  public linkedinUrl: string = LINKEDIN_URL;
+  public githubUrl: string = GITHUB_URL;
 
   constructor() { }
 
---
+++ src/profile.ts
@@ -0,0 +1,6 @@
+const LINKEDIN_URL = "https://www.linkedin.com/in/xuanqi-li/"
+const GITHUB_URL = "https://github.com/LiXuanqi/";
+export {
+  LINKEDIN_URL,
+  GITHUB_URL
+}
